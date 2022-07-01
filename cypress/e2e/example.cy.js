a6ae83f03a1171a515f2aa73e40102e240ad0b13
Minh Pham <minhkhaiv@gmail.com>
2022-07-01 18:27:55 +0700

    first commit

---
+++ cypress/e2e/example.cy.js
@@ -0,0 +1,24 @@
+describe("An example test", () => {
+  it("should visit", () => {
+    cy.visit("https://demo.guru99.com/test/newtours/");
+    cy.wait(10000);
+    cy.get("input[name='userName']").click();
+    cy.get("input[name='userName']").type("tutorial");
+    cy.get("input[name='password']").click();
+    cy.get("input[name='password']").type("tutorial");
+    cy.get("input[name='submit']").click();
+    cy.wait(10000);
+    cy.get("h3").should("be.visible").and("have.text", "Login Successfully");
+    cy.get("h3").contains("Login Successfully");
+    cy.contains("Flights").click();
+    cy.get("select[name='fromMonth']").select("5").should("have.value", "5");
+    cy.get("select[name='fromDay']").select("1").should("have.value", "1");
+    cy.get("select[name='toMonth']").select("6").should("have.value", "6");
+    cy.get("select[name='toDay']").select("30").should("have.value", "30");
+    cy.get("input[name='findFlights']").click();
+    cy.get("font:nth-child(1) > b > font:nth-child(1)").should("be.visible");
+    cy.get("font:nth-child(1) > b > font:nth-child(1)").contains(
+      "After flight finder - No Seats Avaialble"
+    );
+  });
+});
